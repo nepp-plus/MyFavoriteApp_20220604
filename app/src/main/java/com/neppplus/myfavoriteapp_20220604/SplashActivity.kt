8ea297bb2701186d50cdf366b1099cfa39e9dc3e
조경진 <cho881020@gmail.com>
2022-06-04 13:52:18 +0900

    2.5초 딜레이 주기 > Handler 개념 활용

--- app/src/main/java/com/neppplus/myfavoriteapp_20220604/SplashActivity.kt
+++ app/src/main/java/com/neppplus/myfavoriteapp_20220604/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.neppplus.myfavoriteapp_20220604
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,17 @@ class SplashActivity : AppCompatActivity() {
 
 //        로딩화면에서 => (2.5초 기다리고) => 메인화면으로 이동 + 로딩화면 종료
 
-        val myIntent = Intent(this, MainActivity::class.java)
-        startActivity(myIntent)
-        finish()
+        val myHandler = Handler( Looper.getMainLooper() )
+
+        myHandler.postDelayed( {
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+
+        },  2500 )
+
+
 
     }
 }
