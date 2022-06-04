c01c751ae2bc45a0249f84e826e329f187898589
조경진 <cho881020@gmail.com>
2022-06-04 13:49:09 +0900

    로딩 > 메인으로 이동  - 2.5초의 딜레이를 주고 싶은 상황

--- app/src/main/java/com/neppplus/myfavoriteapp_20220604/SplashActivity.kt
+++ app/src/main/java/com/neppplus/myfavoriteapp_20220604/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.myfavoriteapp_20220604
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        로딩화면에서 => (2.5초 기다리고) => 메인화면으로 이동 + 로딩화면 종료
+
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+        finish()
+
     }
 }
